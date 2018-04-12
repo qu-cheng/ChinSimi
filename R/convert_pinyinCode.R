cde87bfcf6daa7ca58775ef93b588b0d35ff8570
Qu Cheng <canalcheng@gmail.com>
2018-04-11 17:50:50 -0700

    Add pinyin code according a blog on CSDN

---
+++ R/convert_pinyinCode.R
@@ -0,0 +1,48 @@
+#' Convert Chinese strings to pinyin.
+#'
+#' @param Chin.str The string need to be converted
+#' @param method Whether the output should be toned or toneless.
+#' @param multi Whether the output should contain more than one pinyin
+#' @param sep Character used to seperate different characters
+#' @param parallel Whether or not use parallel calculation
+#' @return pinyin code of \code{Chin.str} according to the lookup table in [基于音形码的中文字符串相似度算法](https://blog.csdn.net/chndata/article/details/41114771). The code is organized as Consonant + Main vowel + Connection Vowel + Tone. For example, these parts for mian4 is m, an, i, and 4, respectively.
+#' @examples
+#' ChStr2py(c("海上生明月","天涯共此时"))
+
+ChStr2pyc <- function(Chin.strs = "", method = c("toneless", "tone"), multi = FALSE, sep = "_", parallel = FALSE)
+{
+  method <- match.arg(method)
+
+  # Convert a string to pinyin
+  ChStr2py <- function(Chin.str, pylibnew){
+    Chin.char <- unlist(strsplit(Chin.str, split = "")) # divide the string to characters
+
+    # convert a single character to pinyin
+    ChChar2Py <- function(Chin.char){
+      ChCharpy <- pylibnew[[Chin.char]]
+      ChCharpy <- switch(method, tone = ChCharpy, toneless = gsub("[1-4]","", ChCharpy))
+      if(multi){
+        ChCharpy <- ifelse(grepl(",", ChCharpy), paste0("[", ChCharpy, "]"),  ChCharpy)
+      }
+      return(ChCharpy)
+    }
+
+    paste(sapply(Chin.char, ChChar2Py), collapse = sep)
+  }
+
+  # Use parallel computing to convert strings if parallel is TRUE
+  if(parallel)
+  {
+    no_cores <- detectCores() - 1  # Get the number of available string
+    cl <- makeCluster(no_cores)   # Initiate cluster
+    pinyin <- parallel::parSapply(cl, X = Chin.strs, FUN = ChStr2py, pylibnew)
+    stopCluster(cl)
+    return(pinyin)
+  } else {
+    sapply(Chin.strs, ChStr2py, pylibnew)
+  }
+
+}
+
+
+
---
+++ R/pylibnew_dataset.R
@@ -0,0 +1,20 @@
+#' Pinyin database
+#'
+#' A lookup table for pinyin code of Chinese characters
+#'
+#' @docType data
+#'
+#' @usage data(pylib)
+#'
+#' @format An environment with Chinese characters as the names.
+#'
+#' @keywords datasets
+#'
+#'
+#' @source \href{https://blog.csdn.net/chndata/article/details/41114771}{CSDN:基于音形码的中文字符串相似度算法}
+#'
+#' @examples
+#' data(pylibnew)
+#' pylibnew[['程']]
+#'
+"pylibnew"
