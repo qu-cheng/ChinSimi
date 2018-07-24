2e4a31891f4c530f13da68ccc1efb1979f4e42c1
pcollender <pcollender@berkeley.edu>
2018-07-24 09:36:55 -0700

    Update convert_4corner.R

--- R/convert_4corner.R
+++ R/convert_4corner.R
@@ -14,6 +14,7 @@ ChStr2fc <- function(Chin.strs = "", sep = "_", parallel = FALSE)
   # Convert one string to four corner code
   ChStr2fc <- function(Chin.str, FClib){
     Sys.setlocale(category = 'LC_ALL', locale = 'chs')
+    if(is.na(Chin.str)) return(NA)
     Chin.char <- unlist(strsplit(Chin.str, split = "")) # divide the string to characters
 
     # convert a single character to pinyin
